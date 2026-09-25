5a288e65912c4e1950a5509f62c0d1afc28f5631
Xsaintly <deren.julian19@gmail.com>
2026-09-26 01:17:19 +0700

    update

--- Praktikum/TUGAS DASPRO 4 laprak/tipe bentukan garis.py
+++ Praktikum/TUGAS DASPRO 4 laprak/tipe bentukan garis.py
@@ -1,40 +1,26 @@
-def MakeP(x, y):
-    return (x, y)
+type Point = tuple[int, int]
+def MakePoint(x:float, y:float) -> Point:
+    return (x,y)
+def GetAbsis(P: Point) -> float:
+    return P[0]
+def GetOrdinat(P: Point) -> float:
+    return P[1]
+def PanjangGaris (Titik1: Point, Titik2: Point) -> float:
+    return ((GetAbsis(Titik2) - (GetAbsis(Titik1)))**2 + ((GetOrdinat(Titik2) - GetOrdinat(Titik1)))**2)**0.5 
 
-def Pemb(p):
-    return p[0]
+def Gradien(Titik1: Point, Titik2: Point) -> float:
+    return ((GetOrdinat(Titik2) - (GetOrdinat(Titik1))) / (GetAbsis(Titik2) - (GetAbsis(Titik1))))
+def IsSejajar(Titik1: Point, Titik2: Point, Titik3: Point, Titik4: Point) -> float:
+    return(
+        ((Gradien(Titik1, Titik2)) == (Gradien(Titik3, Titik4))) and ((PanjangGaris(Titik1, Titik2)) == (PanjangGaris(Titik3, Titik4)))
+    )
 
-def Peny(p):
-    return p[1]
-
-# Operator (3)
-def AddP(P1, P2):
-    return MakeP(Pemb(P1)*Peny(P2) + Pemb(P2)*Peny(P1), Peny(P1)*Peny(P2))
-
-def SubP(P1, P2):
-    return MakeP(Pemb(P1)*Peny(P2) - Pemb(P2)*Peny(P1), Peny(P1)*Peny(P2))
-
-def MulP(P1, P2):
-    return MakeP(Pemb(P1)*Pemb(P2), Peny(P1)*Peny(P2))
-
-# Predikat (3)
-def IsEqP(P1, P2):
-    return Pemb(P1)*Peny(P2) == Peny(P1)*Pemb(P2)
-
-def IsLtP(P1, P2):
-    return Pemb(P1)*Peny(P2) < Peny(P1)*Pemb(P2)
-
-def IsGtP(P1, P2):
-    return Pemb(P1)*Peny(P2) > Peny(P1)*Pemb(P2)
-
-
-# Contoh pemakaian
-P1 = MakeP(1, 2)
-P2 = MakeP(1, 3)
-
-print("AddP:", AddP(P1, P2))
-print("SubP:", SubP(P1, P2))
-print("MulP:", MulP(P1, P2))
-print("IsEqP?:", IsEqP(P1, P2))
-print("IsLtP?:", IsLtP(P1, P2))
-print("IsGtP?:", IsGtP(P1, P2))
+print (PanjangGaris((0,0), (3,4)))
+print (Gradien((0,0), (3,4)))
+print (IsSejajar(
+    (3,4),
+    (4,10),
+    (5,4),
+    (9,10)
+    
+))
